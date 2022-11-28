114237bbae803b3c4bf94b813f81da7e357181b9
jcalderonc93 <jcalderonc@unal.edu.co>
2022-11-28 08:32:31 -0500

    Accounts modified. TC.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    phone = models.CharField(max_length=20, blank=True)
+    address = models.CharField(max_length=200, blank=True)
+    country = models.CharField(max_length=20, blank=True)
+    state = models.CharField(max_length=20, blank=True)
+    city = models.CharField(max_length=20, blank=True)
+    image = models.ImageField(upload_to='profile_image/', blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username + ' - profile'
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from django.views.generic.list import ListView
+from django.views.generic import DeleteView, CreateView
+
+from .forms import UserEditForm
+from . models import UserProfile
+
+
+class UserSignUp(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('signup_success')
+    template_name = 'accounts/signup.html'
+
+
+class UserLogin(LoginView):
+    template_name = 'accounts/user_login.html'
+    next_page = reverse_lazy('movie_index')
+
+
+class UserLogout(LoginRequiredMixin, LogoutView):
+    template_name = 'accounts/user_logout.html'
+
