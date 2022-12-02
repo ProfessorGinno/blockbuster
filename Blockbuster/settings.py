102080b9e18597c4ce4727715fc297b4580a8a37
jcalderonc93 <jcalderonc@unal.edu.co>
2022-12-02 16:35:43 -0500

    Structure static folders. JC

--- Blockbuster/settings.py
+++ Blockbuster/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
